2ad92f2ccc50d6ae365093604f3163062b65adf9
AndresBenja <abebello@gmail.com>
2024-05-23 22:20:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mybilleteravirtual/Activity_home_page.kt
@@ -0,0 +1,16 @@
+package com.example.mybilleteravirtual
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Activity_home_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+
+    }
+}
